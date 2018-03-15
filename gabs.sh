e8713b95b114e70bb5976770c09a9040d4b51e18
root <bahri.med.zied@gmail.com>
2018-03-15 15:28:02 +0200

    commented echo line 5

--- gabs.sh
+++ gabs.sh
@@ -2,7 +2,7 @@
 while read newline
 do
     new=$(date -d "$( echo "$newline" | awk '{sub(/:[0-9]+\|/,"|",$2);print $1}' )" '+%s' )
-    echo $new
+#    echo $new
     if [ "$old" ] && (( $new - $old > 300))
     then
         printf "%4i seconds gap before %s" "$((new - old))" "$newline"
