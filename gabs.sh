02873626c2fa23da79e3fbe6c67c7450f5cd4220
root <bahri.med.zied@gmail.com>
2018-03-15 16:17:55 +0200

    line counter addded

--- gabs.sh
+++ gabs.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+i=0
+#total=$(cat $1 | wc -l)
+#echo $1
+#total=$(cat $1 | wc -l)
+#echo $total
 while read newline
 do
     new=$(date -d "$( echo "$newline" | awk '{sub(/:[0-9]+\|/,"|",$2);print $1}' )" '+%s' )
@@ -8,4 +13,6 @@ do
         printf "%4i seconds gap before %s" "$((new - old))" "$newline"
     fi
     old=$new
+    let "i++"
+    echo -ne "`echo $i`\r"
 done <$1
