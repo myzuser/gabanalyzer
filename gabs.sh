3ca3038df5aeed5e13efeaf3d0f95f53bd649f83
root <bahri.med.zied@gmail.com>
2018-03-15 17:11:59 +0200

    progress and gab lines printed separately

--- gabs.sh
+++ gabs.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 i=0
-#total=$(cat $1 | wc -l)
+#total=$(cat $1* | wc -l)
+#tot=$(cat $1*)
+#total=$( $tot | wc -l )
 #echo $1
 #total=$(cat $1 | wc -l)
-#echo $total
+#echo $tot
+
 while read newline
 do
     new=$(date -d "$( echo "$newline" | awk '{sub(/:[0-9]+\|/,"|",$2);print $1}' )" '+%s' )
 #    echo $new
     if [ "$old" ] && (( $new - $old > 300))
     then
-        printf "%4i seconds gap before %s" "$((new - old))" "$newline"
+        printf "%4i seconds gap before %s\n" "$((new - old))" "$newline"
     fi
     old=$new
     let "i++"
